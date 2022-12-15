7e3242e0882dba33ec61900acfba47e7e2462c6e
oriell754 <fernand0753@mhs.mdp.ac.id>
2022-12-15 07:54:11 +0700

    p

--- node-API/app.js
+++ node-API/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 // (7) import routes
 const mahasiswaRoutes = require('./routes/calon_mahasiswa')
